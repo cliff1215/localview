2bbcaf8a2f3fc34746b3f53ca9c8df26ae1b4c22
Iksoo Choi <ischoi@techheim.com>
2019-07-12 21:38:07 +0900

    Getting full file path names in the chosen directory recursively by using glob module

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const glob = require('glob');
 const { dialog } = require('electron').remote;
 
 document.getElementById('selFolder').addEventListener('click', (event) => {
 	dialog.showOpenDialog({ properties: [ 'openDirectory' ] }, (filePaths, bookmarks) => {
 		console.log(filePaths);
+		if (filePaths === undefined || filePaths.length === 0) {
+			console.log('The user has clicked the cancle button.');
+			return;
+		}
+		glob(filePaths[0] + '/**/*.dcm', (err, res) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(res);
+			}
+		});
 	});
 });
